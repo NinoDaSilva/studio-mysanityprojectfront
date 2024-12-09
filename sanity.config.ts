70045b0092563ef47cbc7caeeaeafb321c583fb2
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-12-09 08:23:26 +0100

    import singletools plugin

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
+import { singletonTools } from 'sanity-plugin-singleton-tools'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: '4hf66nwd',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), singletonTools()],
 
   schema: {
     types: schemaTypes,
